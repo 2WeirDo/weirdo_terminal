fd08ae0ae717e2918d3f4acbededb53c8ae4d328
zweirdo <1606403438@qq.com>
2023-12-05 15:01:59 +0800

    添加node后台

---
+++ src/server/db/db.js
@@ -0,0 +1,36 @@
+/**
+ * Node.js应用中使用Sequelize库连接到MySQL数据库
+ */
+
+const { Sequelize } = require("sequelize");
+const { dbConfig } = require("./config/getConfig");
+
+/**
+ * 创建数据库实例
+ * @type {Sequelize}
+ */
+const sequelize = new Sequelize({
+  database: dbConfig.database,
+  username: dbConfig.username,
+  password: dbConfig.password,
+  host: dbConfig.host,
+  port: dbConfig.port,
+  dialect: "mysql", // 指定使用的数据库类型
+  logging: console.log, // 指定日志输出函
+});
+
+// 测试连接
+// 使用 .authenticate() 函数测试连接是否正常：
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("MySQL client connected");
+  })
+  .catch((e) => {
+    console.error("Unable to connect to MySQL", e);
+  });
+
+module.exports = sequelize;
+
+// 默认情况下,Sequelize 将保持连接打开状态,并对所有查询使用相同的连接.
+// 如果需要需要关闭连接,请调用 sequelize.close()
---
+++ src/server/model/user.js
@@ -0,0 +1,49 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../db");
+
+/**
+ * 定义用户模型
+ * @author weirdo
+ */
+const UserModel = sequelize.define(
+  "User",
+  {
+    id: {
+      type: DataTypes.BIGINT,
+      autoIncrement: true, // 是否自增
+      primaryKey: true, // 是否为主键
+    },
+    // Model的属性被定义在这里
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false, // 允许为空
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+    },
+    status: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+    createTime: {
+      type: DataTypes.DATE,
+    },
+    updateTime: {
+      type: DataTypes.DATE,
+    },
+  }, // 在定义模型时，还可以指定其他一些属性
+  {
+    tableName: "user", // 指定数据表的名称
+    paranoid: true, // 启用软删除, 指定了 deletedAt 字段的别名为 "isDelete"。
+    deletedAt: "isDelete",
+    timestamps: false, // 不自动生成创建时间和更新时间字段
+  }
+);
+
+module.exports = UserModel;
+
+// console.log(User === sequelize.models.User); // true
