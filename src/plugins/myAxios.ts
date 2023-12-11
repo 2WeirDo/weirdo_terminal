4c452907a566b48bd3d2f181f53604c4b2677e57
zweirdo <1606403438@qq.com>
2023-12-11 11:26:15 +0800

    修改baseURL

--- src/plugins/myAxios.ts
+++ src/plugins/myAxios.ts
@@ -1,6 +1,5 @@
 import axios from 'axios'
 
-
 /**
  *  @ts-ignore 一个特殊的注释，用于告诉TypeScript编译器，忽略某个特定的错误或警告
  * 某些情况下会遇到一些TypeScript编译器无法识别或处理的代码，例如第三方库或旧的代码，
@@ -9,12 +8,19 @@ import axios from 'axios'
  * 可以使用// @ts-ignore注释来告诉编译器忽略这些错误或警告，
  */
 
+// const myAxios = axios.create({
+//   baseURL:
+//   // @ts-ignore
+//     process.env.NODE.ENV === 'production'
+//       ? 'https://express-6y6a-84314-6-1323049144.sh.run.tcloudbase.com/api'
+//       : 'http://localhost:7345/api'
+
+// })
+
+let serverAddress = import.meta.env.VITE_SERVER_ADDRESS
+
 const myAxios = axios.create({
-  baseURL:
-  // @ts-ignore
-    process.env.NODE.ENV === 'production'
-      ? 'https://express-6y6a-84314-6-1323049144.sh.run.tcloudbase.com/api'
-      : 'http://localhost:7345/api'
+  baseURL: `http://${serverAddress}/api`
 })
 
 myAxios.defaults.withCredentials = true
@@ -31,11 +37,11 @@ myAxios.interceptors.request.use(
 myAxios.interceptors.response.use(
   function (response) {
     // console.log(response.data);
-    return response.data;
+    return response.data
   },
-  function(err) {
-    return Promise.reject(err);
+  function (err) {
+    return Promise.reject(err)
   }
 )
 
-export default myAxios;
+export default myAxios
