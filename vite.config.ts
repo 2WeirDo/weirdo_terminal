917ed53207e5c16ed3634f2387545cb435168fce
zweirdo <1606403438@qq.com>
2023-12-14 11:13:36 +0800

    进行项目性能优化

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,14 @@ import Components from 'unplugin-vue-components/vite'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
+// 进行 gzip 压缩
+import compression from 'vite-plugin-compression'
+
+// import { visualizer } from 'rollup-plugin-visualizer'
+
+// 配置 CDN
+import { autoComplete, Plugin as importToCDN } from "vite-plugin-cdn-import";
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,7 +19,35 @@ export default defineConfig({
     vue(),
     Components({
       resolvers: [AntDesignVueResolver()]
-    })
+    }),
+    compression({
+      algorithm: 'gzip', // 压缩算法，可选['gzip'，'brotliCompress'，'deflate'，'deflateRaw']
+      threshold: 10240, // 如果体积大于10kb阈值，则进行压缩，参数单位为b
+      compressionOptions: { level: 9 }, // 指定gzip压缩级别，默认为9（最高级别）
+    }),
+    importToCDN({
+      prodUrl: 'https://unpkg.com/{name}@{path}',
+      modules: [
+        autoComplete('vue'), // 可以自动加速的
+        autoComplete('axios'),
+        {
+          name: 'ant-design-vue',
+          var: 'AntDesignVue',
+          path: '3.2.10',
+          css: '3.2.10/dist/antd.min.css'
+        },
+        {
+          name: 'vue-router',
+          var: 'VueRouter',
+          path: '4.2.5'
+        },
+        {
+          name: 'pinia',
+          var: 'Pinia',
+          path: '2.1.7'
+        }
+      ]
+    }),
   ],
   resolve: {
     alias: {
